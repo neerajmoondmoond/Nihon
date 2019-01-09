49baed16f53d911b79f8769e840db62589b58a4c
neerajmoondmoond <43745771+neerajmoondmoond@users.noreply.github.com>
2019-01-09 17:33:24 +0900

    Update

--- graylog2-web-interface/src/components/common/DocumentTitle.jsx
+++ graylog2-web-interface/src/components/common/DocumentTitle.jsx
@@ -32,7 +32,7 @@ class DocumentTitle extends React.Component {
     document.title = this.defaultTitle;
   }
 
-  defaultTitle = 'NIHON CYBER SIM';
+  defaultTitle = 'NIHON CYBER SIEM';
 
   render() {
     return this.props.children;
